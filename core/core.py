ba9ba16785718fdc292b6490441db932f95ebc8e
Dhruv Pal <paldhruv89@gmail.com>
2020-11-17 20:32:51 +0530

    empty framework ready

--- core/core.py
+++ core/core.py
@@ -3,6 +3,7 @@ import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
 """
+DECOMMENT THIS BLOCK WHEN EACH VAR GETS SET
 range constants TBD for each variable
 cfp_lo =
 cfp_hi =
@@ -12,8 +13,8 @@ bdg_lo =
 bdg_hi =
 wtp_lo =
 wtp_hi =
-psi_lo =
-psi_hi =
+psi_lo = 0
+psi_hi = 100
 """
 
 cfp = ctrl.Antecedent(np.arrange(cfp_lo, cfp_hi, 1), 'carbon_footprint')
@@ -25,21 +26,36 @@ psi = ctrl.Consequent(np.arrange(psi_lo, psi_hi, 1), 'score')
 
 
 #cfp[] = fuzz.trimf(cfp.universe, [])
-cfp['low'] = fuzz.trimf(cfp.universe, [])
-cfp['mdm'] = fuzz.trimf(cfp.universe, [])
-cfp['high'] = fuzz.trimf(cfp.universe, [])
+cfp['L'] = fuzz.trimf(cfp.universe, [cfp_lo, cfp_lo, ])
+cfp['M'] = fuzz.trimf(cfp.universe, [cfp_lo, , cfp_hi])
+cfp['H'] = fuzz.trimf(cfp.universe, [, cfp_hi, cfp_hi])
 
 #rec[] = fuzz.trimf(rec.universe, [])
-rec['low'] = fuzz.trimf(rec.universe, [])
-rec['mdm'] = fuzz.trimf(rec.universe, [])
-rec['high'] = fuzz.trimf(rec.universe, [])
+rec['L'] = fuzz.trimf(rec.universe, [rec_lo, rec_lo, ])
+rec['M'] = fuzz.trimf(rec.universe, [rec_lo, , rec_hi])
+rec['H'] = fuzz.trimf(rec.universe, [ , rec_hi, rec_hi])
 
 #bdg[] = fuzz.trimf(bdg.universe, [])
-bdg['low'] = fuzz.trimf(bdg.universe, [])
-bdg['mdm'] = fuzz.trimf(bdg.universe, [])
-bdg['high'] = fuzz.trimf(bdg.universe, [])
+bdg['L'] = fuzz.trimf(bdg.universe, [bdg_lo, bdg_lo, ])
+bdg['M'] = fuzz.trimf(bdg.universe, [bdg_lo, , bdg_hi])
+bdg['H'] = fuzz.trimf(bdg.universe, [ , bdg_hi, bdg_hi])
 
 #wtp[] = fuzz.trimf(wtp.universe, [])
-wtp['low'] = fuzz.trimf(wtp.universe, [])
-wtp['mdm'] = fuzz.trimf(wtp.universe, [])
-wtp['high'] = fuzz.trimf(wtp.universe, [])
+wtp['L'] = fuzz.trimf(wtp.universe, [wtp_lo, wtp_lo, ])
+wtp['M'] = fuzz.trimf(wtp.universe, [wtp_lo, , wtp_hi])
+wtp['H'] = fuzz.trimf(wtp.universe, [ , wtp_hi, wtp_hi])
+
+#psi[] = fuzz.trapmf(psi.universe, [])
+psi['B'] = fuzz.trapmf(psi.universe, [])
+psi['A'] = fuzz.trapmf(psi.universe, [])
+psi['G'] = fuzz.trapmf(psi.universe, [])
+psi['E'] = fuzz.trapmf(psi.universe, [])
+
+
+#Rule Base
+#ruleX = ctrl.Rule()
+
+
+#ControlSystem
+controller = ctrl.ControlSystem(rule_list)
+generator = ctrl.ControlSystemSimulation(controller)
