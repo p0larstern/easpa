f2d699c8ed66a5bff2dc35239ce6c7299091c78a
Ankur Tyagi <47008577+TWHF@users.noreply.github.com>
2020-12-07 19:41:12 +0530

    Update admin.py

--- PSI/admin.py
+++ PSI/admin.py
@@ -5,7 +5,19 @@ from .models import *
 
 admin.site.register(Customer)
 admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(OrderItem)
-admin.site.register(ShippingAddress)
+class OrderAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_ordered',)
+admin.site.register(Order, OrderAdmin)
+
+class OrderItemAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_added',)
+admin.site.register(OrderItem,OrderItemAdmin)
+
+class ShippingAddressAdmin(admin.ModelAdmin):
+    readonly_fields = ('date_added',)
+admin.site.register(ShippingAddress,ShippingAddressAdmin)
+admin.site.register(Unprocessed_item)
+
+
+
 
