b2adb4a5c67350b1306100fbe05bfe027a53c34d
Dhruv Pal <paldhruv89@gmail.com>
2020-12-09 19:23:12 +0530

    css step 2- admin action added

--- PSI/admin.py
+++ PSI/admin.py
@@ -1,14 +1,45 @@
 from django.contrib import admin
 from PSI.models import Product
+from PSI.models import Order
 from PSI.core import core
+from PSI.core import css as util
 # Register your models here.
 from .models import *
 
-admin.site.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('name', 'css')
+    actions = ['update_css']
+
+    def update_css(self, request, queryset):
+        for cust in queryset:
+            order_list = Order.objects.filter(complete=True).filter(customer = cust)
+            print(order_list)
+            used_order_list = order_list.filter(used = True)
+            unused_order_list = order_list.filter(used = False)
+            total_psi = 0
+            order_cnt = len(used_order_list)
+            for order in used_order_list:
+                total_psi += order.Average_PSS
+            avg_psi = 0
+            if order_cnt > 0:
+                avg_psi = total_psi / order_cnt
+            tcss = cust.css
+            for order in unused_order_list:
+                tcss = util.get_css(tcss, avg_psi, order_cnt, order.Average_PSS)
+                total_psi += order.Average_PSS
+                order_cnt += 1
+                avg_psi = total_psi / order_cnt
+                order.used = True
+                order.save()
+            cust.css = tcss
+            cust.save()
+
+admin.site.register(Customer, CustomerAdmin)
 #admin.site.register(Product)
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ('date_ordered',)
-admin.site.register(Order, OrderAdmin)
+    list_display = ('customer','used')
+admin.site.register(Order,OrderAdmin)
 
 class OrderItemAdmin(admin.ModelAdmin):
     readonly_fields = ('date_added',)
@@ -32,7 +63,7 @@ class ProductAdmin(admin.ModelAdmin):
             item.pss = g.get_psi(
                 item.carbon_footprint,
                 item.recyclability,
-                item.water_used,
+                item.biodegradability,
                 item.waste_treated
             )
             item.listed = 'y'
